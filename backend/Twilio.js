eeca5c4473dd7b3a6f1423b5d8cde0aa26b86a7a
Thayana Mamoré <thayana.mamore@topi.io>
2020-11-12 11:42:29 -0300

    feat: Created a Twilio Verify Service

--- backend/Twilio.js
+++ backend/Twilio.js
@@ -3,3 +3,4 @@ const phoneNumberSid = process.env.PHONE_NUMBER_SID;
 const tokenSid = process.env.API_SID;
 const tokenSecret = process.env.API_SECRET;
 const accountSid = process.env.ACCOUNT_SID;
+const verify = process.env.VERIFY_SERVICE_ID;
