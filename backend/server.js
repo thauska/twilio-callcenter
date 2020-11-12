6686e344e2769f015fc05bae81ed3c8c8bcbc753
Thayana Mamoré <thayana.mamore@topi.io>
2020-11-12 11:57:16 -0300

    feat: Started express server

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.get('/test', (req, res) => {
+    res.send('Welcome to Twilio!!!')
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`)
+});
