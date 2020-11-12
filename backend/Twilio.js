a1bf5b57503424736617a0ccebac78348f37ba43
Thayana Mamoré <thayana.mamore@topi.io>
2020-11-12 14:26:46 -0300

    feat: created endpoints to receive mobile number and the verification code. Added environment variables manager.

--- backend/Twilio.js
+++ backend/Twilio.js
@@ -1,6 +1,10 @@
+require('dotenv/config')
+
 const phoneNumber = process.env.PHONE_NUMBER;
 const phoneNumberSid = process.env.PHONE_NUMBER_SID;
 const tokenSid = process.env.API_SID;
 const tokenSecret = process.env.API_SECRET;
 const accountSid = process.env.ACCOUNT_SID;
 const verify = process.env.VERIFY_SERVICE_ID;
+
+console.log(phoneNumber, phoneNumberSid, tokenSid, tokenSecret, accountSid, verify )
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv/config')
 const express = require('express');
 const app = express();
 const PORT = 3001;
@@ -5,6 +6,16 @@ const PORT = 3001;
 app.get('/test', (req, res) => {
     res.send('Welcome to Twilio!!!')
 });
+
+app.get('/login', (req, res) => {
+    console.log('loging in ')
+});
+
+app.get('/verify', (req, res) => {
+    console.log('Verifing code ')
+});
+
+console.log(process.env.MOBILE_PHONE_NUMBER)
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`)
